122cdaedc6fedde0133813ca00da83090f429228
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-05-31 22:55:04 +0600

    authentication without jwt done

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
 import Home from "./../../pages/Home/Home";
 import ErrorPage from "../../pages/ErrorPage/ErrorPage";
+import Login from "../../pages/Login/Login";
+import SignUp from "../../pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -15,4 +17,6 @@ export const router = createBrowserRouter([
       },
     ],
   },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <SignUp></SignUp> },
 ]);
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,58 +1,75 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 import toast from "react-hot-toast";
-import useAuth from "./../../components/hooks/useAuth";
-const Login = () => {
+import { useState } from "react";
+import useAuth from "../../components/hooks/useAuth";
+
+const SignUp = () => {
+  const [registerError, setRegisterError] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
-  const { signInWithGoogle, signIn, user } =
-    useAuth();
-  useEffect(() => {
-    if (user) {
-      navigate("/");
-    }
-  }, [navigate, user]);
   const from = location.state || "/";
-  const handleGoogleSignIn = async () => {
-    try {
-      const result = await signInWithGoogle();
-      // console.log(result.user);
-      toast.success("Sign-in Successfully");
-      navigate(from, { replace: true });
-    } catch (err) {
-      // console.log(err);
-      toast.error("something went wrong!");
-    }
-  };
-
-  const handleSignIn = async (e) => {
+  const { createUser, signInWithGoogle, updateUserProfile, setUser } =
+    useAuth();
+  const handleSignUp = async (e) => {
     e.preventDefault();
     const form = e.target;
     const email = form.email.value;
+    const name = form.name.value;
+    const photo = form.photo.value;
     const pass = form.password.value;
-    // console.log({ email, pass });
+    // console.log({ email, pass, name, photo });
+
+    setRegisterError("");
+
+    if (pass.length < 6) {
+      setRegisterError("Password should be at least 6 characters or longer");
+      return;
+    } else if (!/[A-Z]/.test(pass)) {
+      setRegisterError(
+        "Your password should have at least one uppercase character."
+      );
+      return;
+    } else if (!/[!@#$%^&*]/.test(pass)) {
+      setRegisterError(
+        "Your password should have at least one special character."
+      );
+      return;
+    } else if (!/\d/.test(pass)) {
+      setRegisterError(
+        "Your password should have at least one numeric character."
+      );
+      return;
+    }
+
     try {
-      const result = await signIn(email, pass);
-      // console.log(result.user);
-      // console.log(data);
+      const result = await createUser(email, pass);
+
+      await updateUserProfile(name, photo);
+      setUser({ ...result?.user, photoURL: photo, displayName: name });
+
+      navigate(from, { replace: true });
+      toast.success("Sign-up Successful");
+    } catch (err) {
+      // console.log(err);
+      toast.error(err?.message);
+    }
+  };
+
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithGoogle();
+      //   console.log(result);
+
       toast.success("Sign-in Successful");
       navigate(from, { replace: true });
     } catch (err) {
-      // console.log(err);
-      toast.error("please enter a valid email or password!");
+      toast.error(err?.message);
     }
   };
-  if (user) return;
-  return (
-    <div className="flex justify-center items-center min-h-[calc(100vh-306px)] my-12">
-      <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl ">
-        {/* <div
-          className="hidden bg-cover bg-center lg:block lg:w-1/2"
-          style={{
-            backgroundImage: `url(${bgImg})`,
-          }}
-        ></div> */}
 
+  return (
+    <div className="flex justify-center items-center h-[500px] my-20">
+      <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white border rounded-lg shadow-lg  lg:max-w-4xl ">
         <div className="w-full mx-auto px-6 py-8 md:px-8 lg:w-1/2">
           <div
             onClick={handleGoogleSignIn}
@@ -80,20 +97,53 @@ const Login = () => {
             </div>
 
             <span className="w-5/6 px-4 py-3 font-bold text-center">
-              Sign in with Google
+              Continue with Google
             </span>
           </div>
 
           <div className="flex items-center justify-between mt-4">
             <span className="w-1/5 border-b  lg:w-1/4"></span>
 
-            <div className="text-xs text-center text-gray-500 uppercase  hover:underline">
-              or login with email
+            <div className=" text-center text-gray-500   hover:underline">
+              or Registration with Email
             </div>
 
             <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
           </div>
-          <form onSubmit={handleSignIn}>
+          <form onSubmit={handleSignUp}>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600 "
+                htmlFor="name"
+              >
+                UserName
+              </label>
+              <input
+                id="name"
+                autoComplete="name"
+                placeholder="UserName"
+                name="name"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+                required
+              />
+            </div>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600 "
+                htmlFor="photo"
+              >
+                Photo URL
+              </label>
+              <input
+                id="photo"
+                autoComplete="photo"
+                placeholder="Photo URL"
+                name="photo"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+              />
+            </div>
             <div className="mt-4">
               <label
                 className="block mb-2 text-sm font-medium text-gray-600 "
@@ -103,8 +153,8 @@ const Login = () => {
               </label>
               <input
                 id="LoggingEmailAddress"
-                placeholder="Email Address"
                 autoComplete="email"
+                placeholder="Email Address"
                 name="email"
                 className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
                 type="email"
@@ -125,7 +175,7 @@ const Login = () => {
               <input
                 id="loggingPassword"
                 autoComplete="current-password"
-                placeholder="Password"
+                placeholder="Give a Strong Password"
                 name="password"
                 className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
                 type="password"
@@ -137,19 +187,21 @@ const Login = () => {
                 type="submit"
                 className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
               >
-                Sign In
+                Sign Up
               </button>
             </div>
           </form>
+
+          {registerError && <p className="p-4 text-red-600">{registerError}</p>}
 
           <div className="flex items-center justify-between mt-4">
             <span className="w-1/5 border-b  md:w-1/4"></span>
 
             <Link
-              to="/register"
+              to="/login"
               className="text-xs text-gray-500 uppercase  hover:underline"
             >
-              or sign up
+              or sign in
             </Link>
 
             <span className="w-1/5 border-b  md:w-1/4"></span>
@@ -160,4 +212,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
