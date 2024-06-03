57c96895e159342c2ec000886383a03a9e9bee23
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-06-03 22:27:23 +0600

    footer done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../../components/home/banner/Banner";
 import PopularContests from "../../components/home/popularContest/PopularContext";
+import Footer from "../../components/home/footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <PopularContests></PopularContests>
+      <Footer></Footer>
     </div>
   );
 };
