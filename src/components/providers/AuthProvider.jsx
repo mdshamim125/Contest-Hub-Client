6f06cc3776f7482dd765e352eae5702e67a4e833
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-06-07 22:13:39 +0600

    token verification done in client side

--- src/components/providers/AuthProvider.jsx
+++ src/components/providers/AuthProvider.jsx
@@ -12,6 +12,7 @@ import {
 } from "firebase/auth";
 import { app } from "./firebase/firebase.config";
 import axios from "axios";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
@@ -19,6 +20,7 @@ const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
 
   const createUser = (email, password) => {
     setLoading(true);
@@ -59,10 +61,7 @@ const AuthProvider = ({ children }) => {
       role: "user",
       status: "Verified",
     };
-    const { data } = await axios.put(
-      `${import.meta.env.VITE_API_URL}/user`,
-      currentUser
-    );
+    const { data } = await axiosPublic.put(`/user`, currentUser);
     return data;
   };
 
@@ -72,8 +71,19 @@ const AuthProvider = ({ children }) => {
       setUser(currentUser);
       if (currentUser) {
         saveUser(currentUser);
+        // get token and store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+            setLoading(false);
+          }
+        });
+      } else {
+        // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+        localStorage.removeItem("access-token");
+        setLoading(false);
       }
-      setLoading(false);
     });
     return () => {
       return unsubscribe();
