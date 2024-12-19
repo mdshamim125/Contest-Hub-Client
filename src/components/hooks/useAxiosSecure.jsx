eb445fcbe7c2786c38b32fe3fb6f4aad0152e3cd
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-12-19 21:27:33 +0600

    base url changed

--- src/components/hooks/useAxiosSecure.jsx
+++ src/components/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:5000'
-  // baseURL: "https://contest-hub-server-red.vercel.app",
+  // baseURL: 'http://localhost:5000'
+  baseURL: "https://contest-hub-server-red.vercel.app",
 });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
